67b500f2b11d341d1b8d661d2455a52baba617d8
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-26 18:44:25 -0500

    adding comments

--- src/components/buttonComponent.jsx
+++ src/components/buttonComponent.jsx
@@ -1,3 +1,27 @@
+// in class we had the following code showing a series of buttons
+
+        // <button
+        //   onClick={() => welcomeStudent("Mary")}
+        //   className="list-group-item list-group-item-action">
+        //   Mary
+        // </button>
+
+        // <button
+        //   onClick={() => welcomeStudent("Cole")}
+        //   className="list-group-item list-group-item-action">
+        //   Cole
+        // </button>
+
+        // <button
+        //   onClick={() => welcomeStudent("Rebecca")}
+        //   className="list-group-item list-group-item-action">
+        //   Rebecca
+        // </button>
+
+// with React, we can make our own custom button component and give it the data it needs to do the job we want it to do
+// this saves us frm having to write out the same code over and over again and gives us a reusable component that we can use
+// elsewhere in our application
+
 export default function ButtonComponent({ name, instrument }) {
 
   const buttonClick = () => {
@@ -13,6 +37,7 @@ export default function ButtonComponent({ name, instrument }) {
   };
 
   return (
+      // we only have to write the button once and we can reuse it as many times as we want
       <button 
         onClick={buttonClick}
       >{name}
--- src/pages/SamplePage.jsx
+++ src/pages/SamplePage.jsx
@@ -1,3 +1,29 @@
+// in class we had the following code showing a series of buttons
+
+        // <button
+        //   onClick={() => welcomeStudent("Mary")}
+        //   className="list-group-item list-group-item-action">
+        //   Mary
+        // </button>
+
+        // <button
+        //   onClick={() => welcomeStudent("Cole")}
+        //   className="list-group-item list-group-item-action">
+        //   Cole
+        // </button>
+
+        // <button
+        //   onClick={() => welcomeStudent("Rebecca")}
+        //   className="list-group-item list-group-item-action">
+        //   Rebecca
+        // </button>
+
+// with React, we can make our own custom button component and give it the data it needs to do the job we want it to do
+// this saves us frm having to write out the same code over and over again and gives us a reusable component that we can use
+// elsewhere in our application
+
+// two ways to pass data to a component are displayed on the sampe page below
+
 // import components
 import ButtonComponent from "../components/buttonComponent";
 // import data
